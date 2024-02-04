c11c4ff341d73eebf186569607ee59608a982efe
Amarildo <amarildokola36@gmail.com>
2024-02-05 00:28:14 +0100

    little browser bug fixed

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      <h1>Welcome to this Nextjs food app!</h1>
+      <p>🔥 Let&apos;s goo! 🔥</p>
     </main>
   );
 }
