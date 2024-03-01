7b2b22df40cb74c03b0fbe9ec0fe3e235041ca4d
Amarildo <amarildokola36@gmail.com>
2024-03-01 20:01:26 +0100

    other routes added

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "NextJS Course App",
+  title: "NextJS Food App",
   description: "Your first NextJS app!",
 };
 
--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this Nextjs food app!</h1>
       <p>🔥 Let&apos;s goo! 🔥</p>
+      <p>
+        <Link href="/about">About Us</Link>
+      </p>
     </main>
   );
 }
