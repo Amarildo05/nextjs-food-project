46fc8f825dc9105d7cf20486851ffc9a1da39bf1
Amarildo <amarildokola36@gmail.com>
2024-03-01 21:13:04 +0100

    some adj...

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this Nextjs food app!</h1>
+      <h1>NextJs Dynamic Routes/Routes Parameters Demo!</h1>
     </div>
   );
 }
